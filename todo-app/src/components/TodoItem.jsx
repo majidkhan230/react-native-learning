abeddad7d0752aa22840266b29bf5d08d1236fff
MajidKh230 <khanmajid230@gmail.com>
2024-11-09 22:29:42 +0500

    add delete icon

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,28 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+const TodoItem = ({ item, pressHandler }) => {
+  //    const handleClick = ()=>(
+  //     console.log("clicked on item")
+  //    )
 
-const TodoItem = ({item,pressHandler}) => {
-
-//    const handleClick = ()=>(
-//     console.log("clicked on item")
-//    ) 
-    
   return (
-    <TouchableOpacity onPress={()=>(pressHandler(item.id))} >
-      <Text style={styles.item}>{item.title}</Text>
-    </TouchableOpacity >
-  )
-}
+    <TouchableOpacity onPress={() => pressHandler(item.id)}>
+      <View  style={styles.item}>
+        <MaterialIcons name="delete" size={18} color="#333"/>
+        <Text>{item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
 
 const styles = StyleSheet.create({
-    item:{
-        padding:16,
-        marginTop:16,
-        borderStyle:'dashed',
-        borderWidth:1,
-        borderRadius:10,
-    }
-})
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderStyle: "dashed",
+    borderWidth: 1,
+    borderRadius: 10,
+    flexDirection:"row",
+    gap:4,
+  },
+});
