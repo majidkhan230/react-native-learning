952b4b6c0ad7b04f1d47d91c2ad1a68ab43f5222
MajidKh230 <khanmajid230@gmail.com>
2024-11-14 22:05:56 +0500

    add icons

--- StickerSmash/src/app/(tabs)/_layout.jsx
+++ StickerSmash/src/app/(tabs)/_layout.jsx
@@ -1,20 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
-
+import { Ionicons } from "@expo/vector-icons";
 const TabLayout = () => {
   return (
-    <Tabs>
+    <Tabs >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+          tabBarIcon:({focused,color})=>(
+            <Ionicons name={focused ? "home-sharp" : "home-outline"}  color={color} size={24} ></Ionicons>
+          )
         }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="about"
         options={{
           title: "About",
+          tabBarIcon:({focused,color})=>(
+            <Ionicons name={focused ? "information-circle" : "information-circle-outline"} color={color} size={24}></Ionicons>
+          )
         }}
       ></Tabs.Screen>
     </Tabs>
