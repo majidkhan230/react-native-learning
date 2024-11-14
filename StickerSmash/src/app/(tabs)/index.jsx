a1572c5890fb8f09f4d14a292fcfb943dced92e3
MajidKh230 <khanmajid230@gmail.com>
2024-11-14 22:30:05 +0500

    add images

--- StickerSmash/src/app/(tabs)/index.jsx
+++ StickerSmash/src/app/(tabs)/index.jsx
@@ -1,34 +1,49 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const PlaceHolderImage = require('@/src/assets/images/background-image.png')
+
 const index = () => {
   return (
-  <View style={styles.container}>
-    <Text  style={styles.text}>Hello world</Text>
- <Link href={"mjd"} style={styles.button}  >
-<Text >Go to About Screen</Text>
- </Link>
-  </View>
-
-
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>Hello world</Text>
+      <Link href={"mjd"} style={styles.button}>
+        <Text>Go to About Screen</Text>
+      </Link> */}
+      <View style={styles.imageContainer}>
+        <Image source={PlaceHolderImage} style={styles.image}></Image>
+      </View>
+    </View>
   );
 };
 
 export default index;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    color: "#fff",
+    textDecorationLine: "underline",
+  },
+  imageContainer:{
     flex:1,
-    backgroundColor:"#25292e",
-    alignItems:"center",
-    justifyContent:"center",
+    // backgroundColor:"red",
   },
-  text:{
-    color:"#fff",
-  },
-  button:{
-    fontSize:20,
-    color:"#fff",
-    textDecorationLine:"underline",
+  image:{
+    width:320,
+    height:400,
+    // backgroundColor:"red",
+    borderRadius:18,
   }
 });
