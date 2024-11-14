bffb99b78a8597145e7dc7fb678eb91e293a21f1
MajidKh230 <khanmajid230@gmail.com>
2024-11-14 22:13:59 +0500

    add style

--- StickerSmash/src/app/(tabs)/_layout.jsx
+++ StickerSmash/src/app/(tabs)/_layout.jsx
@@ -4,23 +4,45 @@ import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 const TabLayout = () => {
   return (
-    <Tabs >
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle:{
+            backgroundColor:"#25292e",
+        },
+        headerTintColor:"#fff",
+        headerShadowVisible:false,
+        tabBarStyle:{
+            backgroundColor:"#25292e",
+        }
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon:({focused,color})=>(
-            <Ionicons name={focused ? "home-sharp" : "home-outline"}  color={color} size={24} ></Ionicons>
-          )
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            ></Ionicons>
+          ),
         }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="about"
         options={{
           title: "About",
-          tabBarIcon:({focused,color})=>(
-            <Ionicons name={focused ? "information-circle" : "information-circle-outline"} color={color} size={24}></Ionicons>
-          )
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              color={color}
+              size={24}
+            ></Ionicons>
+          ),
         }}
       ></Tabs.Screen>
     </Tabs>
