f0cdad22d77d2a2cbaf3a029ab168e3e094f3f98
MajidKh230 <khanmajid230@gmail.com>
2024-11-19 23:07:42 +0500

    add assets and work on tabs

--- Aora/src/app/(tabs)/_layout.jsx
+++ Aora/src/app/(tabs)/_layout.jsx
@@ -1,16 +1,95 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="jusitfy-center items-center gap-1 ">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      ></Image>
+      <Text
+        className={`text-sm  `}
+        style={{
+          color: color,
+        }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabLayout = () => {
   return (
- <Tabs>
-    <Tabs.Screen name='home'></Tabs.Screen>
-    <Tabs.Screen name='create'></Tabs.Screen>
-    <Tabs.Screen name='bookmark'></Tabs.Screen>
-    <Tabs.Screen name='profile'></Tabs.Screen>
- </Tabs>
-  )
-}
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              focused={focused}
+              name="Home"
+            />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="create"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              icon={icons.plus}
+              color={color}
+              focused={focused}
+              name="Create"
+            />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              icon={icons.profile}
+              color={color}
+              focused={focused}
+              name="Profile"
+            />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              icon={icons.bookmark}
+              color={color}
+              focused={focused}
+              name="Bookmark"
+            />
+          ),
+        }}
+      ></Tabs.Screen>
+    </Tabs>
+  );
+};
 
-export default TabLayout
+export default TabLayout;
--- Aora/src/app/index.jsx
+++ Aora/src/app/index.jsx
@@ -6,7 +6,7 @@ const index = () => {
   return (
     <View className="bg-orange-600 flex-1 items-center justify-center">
       <Text className="text-teal-400 font-bold text-5xl">index</Text>
-      <Button title='go to profile' onPress={()=>navigation.push("profile")}/>
+      <Button title='go to Home' onPress={()=>navigation.push("(tabs)")}/>
     </View>
   )
 }
